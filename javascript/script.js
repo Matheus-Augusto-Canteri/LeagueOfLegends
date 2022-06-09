15050731f7d2f554ef45ee0415fa7db4ac5faae3
RoronoaTheuzo <101112856+RoronoaTheuzo@users.noreply.github.com>
2022-06-09 17:28:57 -0300

    updateLeagueOfLegends

--- javascript/script.js
+++ javascript/script.js
@@ -3,21 +3,20 @@ const button = document.querySelector('.loginButton');
 
 const handleFocus = ({ target }) => {
     const span = target.previousElementSibling;
-    span.classList.add('spanActive');
+    span.classList.add('span-active');
 }
 
 const handleFocusOut = ({ target }) => {
-
-    if (target.value == '') {
+    if (target.value === '') {
         const span = target.previousElementSibling;
-        span.classList.remove('spanActive');
+        span.classList.remove('span-active');
     }
-
 }
+
 const handleChange = () => {
     const [username, password] = inputs;
 
-    if (username.value && password.value.length >= 8){
+    if (username.value && password.value.length >= 8) {
         button.removeAttribute('disabled');
     } else {
         button.setAttribute('disabled', '');
